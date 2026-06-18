0fde6d56cc9b96bb9f0d8808c9baf2a9e4ed5776
SimonD0809 <Simon.83@outlook.fr>
2026-06-18 16:02:44 +0200

    jjj

---
+++ index.js
@@ -0,0 +1,43 @@
+// const supp = document.querySelector(".test-delete");
+// const le = document.querySelector(".ecrit");
+// const add = document.querySelector(".test-add");
+// console.log("test");
+
+// add.addEventListener("click", () => {
+//   le.innerHTML = "nouveau test";
+//   supp.style.display = "block";
+//   add.style.display = "none";
+// });
+
+// supp.addEventListener("click", () => {
+//   le.innerHTML = "";
+//   add.style.display = "block";
+//   supp.style.display = "none";
+// });
+
+const plus = document.querySelector(".plus");
+const moins = document.querySelector(".moins");
+const number = document.querySelector(".number");
+const body = document.querySelector("body");
+let str = "0";
+let nombre = Number(str);
+number.innerHTML = nombre;
+
+console.log(nombre);
+
+plus.addEventListener("click", () => {
+  number.innerHTML = nombre++;
+  if (nombre > 10) {
+    body.style.backgroundColor = "red";
+  }
+});
+
+moins.addEventListener("click", () => {
+  number.innerHTML = nombre--;
+  if (nombre <= 0) {
+    nombre = 0;
+  }
+  if (nombre < 10) {
+    body.style.backgroundColor = "white";
+  }
+});
